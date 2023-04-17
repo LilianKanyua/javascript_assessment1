d583c88aab87a6e513b68c0f72fd752d5a0ba6a7
LilianKanyua <liliankanyua@gmail.com>
2023-04-17 14:28:23 +0300

    commit

--- assessment1.js
+++ assessment1.js
@@ -3,7 +3,7 @@ function reverseString(food){
    let reverse= food.split("").reverse().join("");
    return reverse;
 }
-let food="eat";
+let food="eating";
 console.log(reverseString(food));
   
 // question two
@@ -32,20 +32,20 @@ function merging(left,right){
     return [...emptyArr,...left,...right];
 }
 
-let num=[2,8,0,23,5,45,76];
-console.log(merge(num));
+// let num=[2,8,0,23,5,45,76];
+// console.log(merge(num));
 // finding the target
 
 
-function locateTarget(num2,target){
+function locateTarget(num,target){
     let leftSide=0;
-    let rightSide=num2.length -1;
+    let rightSide=num.length -1;
     while(leftSide<=rightSide){
         let midArr=Math.floor((leftSide+rightSide)/2);
-  if(num2[midArr]===target){
+  if(num[midArr]===target){
       return midArr;
   }
-  else if(num2[midArr]>target){
+  else if(num[midArr]>target){
       rightSide=midArr -1;
   }
   else{
@@ -55,31 +55,73 @@ function locateTarget(num2,target){
 }
 return null;
 }
-let num2=[0,2,5,8,23,45,76]
+
 let target=23;
-console.log(locateTarget(num2,target));
-// question five
-function multiply(arrNum){
-   
-    for(let i=0; i<arrNum.length; i++){
-        
-        console.log(arrNum[i]*4);
+let num=merge([2,8,0,23,5,45,76])
+console.log(locateTarget(num,target));
+
+// Question three
+function leapYears(){
+    for(let year=2000;year <2023;year++){
+        if(year%4===0){
+            console.log(year+" is a leap year")
+        }
+        else{
+            console.log(year+" is not a leap year");
+        }
     }
 }
-let arrNum=[12,87,45,75,23,64,73];
+leapYears();
 
-multiply(arrNum);
+// Question four
+
+function myNumbers(){
+for(let number=0; number<100; number++){
+    if(number%3===0 && number%5===0){
+    console.log("FizzBuzz")
+    }
+    else if(number%3===0){
+        console.log("Fizz")
+    }
+    else if(number%5===0){
+        console.log("Buzz")
+    }
+    else{
+    //   console.log(number);
+    }
+}
+    
+}
+myNumbers();
+
+// question five
+let newArr=[];
+function multiply(arrNum){
+    for(let i=0; i< arrNum.length; i++){
+ newArr.push(arrNum[i]*4);
+    }
+return newArr;
+}
+let arrNum=[12,87,45,75,23,64,73];
+ console.log(multiply(arrNum));
+
+
+
 
 // question six
 
 function turnToNumbers(nums){
+    let newArr=[];
 for(let i=0; i<nums.length; i++){
-        
-        console.log(Number(nums[i]));
+    newArr.push(Number(nums[i]));
 }
-}
+return newArr;
+};
 let nums=["10","24","45","56","67"];
 console.log(turnToNumbers(nums));
+// function turnToNumbers(nums){
+    
+// }
 
 // function leapYears(years){
 //     for(i = 2000;i<=2023;i++){
